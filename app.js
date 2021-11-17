b3d7fc06734e33844b8b63db70c8ba271175bb4b
Roni Akiola <roniak@metropolia.fi>
2021-11-17 09:49:52 +0200

    modified:   app.js 	modified:   package-lock.json 	modified:   package.json 	new file:   utils/pass.js

--- app.js
+++ app.js
@@ -2,17 +2,33 @@
 const cookieParser = require('cookie-parser');
 const express = require('express');
 const session = require('express-session');
+const passport = require('./utils/pass');
 const app = express();
 const port = 3000;
+
+const loggedIn = (req, res, next) => {
+  if (req.user) {
+    next();
+  } else {
+    res.redirect('/form');
+  }
+};
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
-const username = 'foo';
-const password = 'bar';
-
 app.use(cookieParser());
-app.use(session({ secret: 'ihan sama', cookie: { maxAge: 60000 }}));
+app.use(session
+  ({
+    secret: 'ihan sama',
+    cookie: { maxAge: 60000 },
+    resave: false,
+    saveUninitialized: true
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.set('views', './views');
 app.set('view engine', 'pug');
@@ -25,28 +41,18 @@ app.get('/form', (req, res) => {
   res.render('form');
 });
 
-app.post('/login', (req, res) => {
-  console.log(req.body);
-  if(req.body.password === password && req.body.username === username){
-    req.session.logged = true;
-    res.redirect('/secret');
-  }else{
-    req.session.logged = false;
-    res.redirect('/form');
-  }
+app.post('/login', passport.authenticate('local', {failureRedirect: '/form'}), (req, res) => {
+  console.log('success');
+  res.redirect('/secret');
 });
 
 app.get('/logout', (req, res) => {
-  req.session.destroy();
+  req.logout();
   res.redirect('/');
 });
 
-app.get('/secret', (req, res) => {
-  if(req.session.logged){
-    res.render('secret');
-  }else{
-    res.redirect('/form');
-  }
+app.get('/secret', loggedIn, (req, res) => {
+  res.render('secret');
 });
 
 app.get('/setCookie/:clr', (req, res) => {
